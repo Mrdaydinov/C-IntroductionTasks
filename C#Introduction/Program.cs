cf9732b9dc00b21d03834e02bf8ded6e8bd19fba
Murad <mrdaydinov@gmail.com>
2024-03-14 10:14:45 +0400

    init commit

---
+++ C#Introduction/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace C_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 2, 3, 12, 15, 19, 29, 14, 25, 26 };
+
+        input:
+            Console.WriteLine("Enter your age: ");
+            bool check = int.TryParse(Console.ReadLine(), out int inputNum);
+
+            if (!check)
+            {
+                Console.WriteLine("Not number");
+                goto input;
+            }
+
+            bool output = false;
+            foreach (int i in arr)
+            {
+                if (i == inputNum)
+                {
+                    output = true;
+                    break;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
