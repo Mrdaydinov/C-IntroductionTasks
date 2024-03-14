edf0b6adbde7a9d0a4d6ee4cf90a8222f241b434
Murad <mrdaydinov@gmail.com>
2024-03-14 10:23:47 +0400

    second task

--- C#Introduction/Program.cs
+++ C#Introduction/Program.cs
@@ -5,28 +5,51 @@
         static void Main(string[] args)
         {
 
-            int[] arr = { 1, 2, 3, 12, 15, 19, 29, 14, 25, 26 };
+            //    int[] arr = { 1, 2, 3, 12, 15, 19, 29, 14, 25, 26 };
+
+            //input:
+            //    Console.WriteLine("Enter your age: ");
+            //    bool check = int.TryParse(Console.ReadLine(), out int inputNum);
+
+            //    if (!check)
+            //    {
+            //        Console.WriteLine("Not number");
+            //        goto input;
+            //    }
+
+            //    bool output = false;
+            //    foreach (int i in arr)
+            //    {
+            //        if (i == inputNum)
+            //        {
+            //            output = true;
+            //            break;
+            //        }
+            //    }
+            //    Console.WriteLine(output);
+
+
+            string[] students = { "Eli", "Veli", "Pirveli", "Kenan", "Hikmet" };
 
         input:
-            Console.WriteLine("Enter your age: ");
-            bool check = int.TryParse(Console.ReadLine(), out int inputNum);
+            Console.WriteLine("Enter student name");
+            string name = Console.ReadLine();
 
-            if (!check)
+
+            bool flag = false;
+            foreach (string student in students)
             {
-                Console.WriteLine("Not number");
+                if (name == student)
+                    flag = true;
+            }
+
+            if (flag)
+                Console.WriteLine(name);
+            else
+            {
+                Console.WriteLine("Student not found");
                 goto input;
             }
-
-            bool output = false;
-            foreach (int i in arr)
-            {
-                if (i == inputNum)
-                {
-                    output = true;
-                    break;
-                }
-            }
-            Console.WriteLine(output);
         }
     }
 }
